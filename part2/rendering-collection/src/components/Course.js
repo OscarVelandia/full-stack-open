d8d8a8eaa1273f3e798fc234b9563369571ecfbe
OscarVelandia <odvs12@gmail.com>
2019-10-12 20:46:08 -0300

    dinamic rendering in the "rendering-collection" exercise

--- part2/rendering-collection/src/components/Course.js
+++ part2/rendering-collection/src/components/Course.js
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+
+const Header = ({ course }) => <h2>{course}</h2>;
+
+const Content = ({ content }) => (
+  <div>
+    {content.map(item => {
+      return <Part name={item.name} exercise={item.exercises} key={item.id} />;
+    })}
+  </div>
+);
+
+const Part = ({ name, exercise }) => (
+  <p>
+    {name} {exercise}
+  </p>
+);
+
+const Total = ({ total }) => {
+  const totalExercises = total.reduce(
+    (acumulator, current) => current.exercises + acumulator,
+    0
+  );
+
+  return <p>Total of exercises {totalExercises}</p>;
+};
 
 const Course = ({ course }) => (
   <div>
-    <Header course={course} />
+    <Header course={course.name} />
+    <Content content={course.parts} />
+    <Total total={course.parts} />
   </div>
 );
 
